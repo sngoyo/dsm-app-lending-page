40b6ebca467f5c1b2812521263e3c046ce771831
sngoyo <salim@kali>
2023-05-29 02:40:48 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import NavBar from "./navBar";
 import './App.css';
+import { useSelector } from "react-redux";
+import PhotoComponent from "./photoComponent";
 
 function App() {
+  const data = useSelector((state => state.allPhotos));
+  console.log(data)
+  const pic = data[0].src
+
+  const appStyle = {
+    width: "100%",
+    height: "100%",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <style>{`body { background-size: cover;}`}</style>
+        <style>{`body {background-image: url(${pic})}`}</style>
+           <NavBar />
+           <PhotoComponent />
+      </div>
   );
 }
-
 export default App;
