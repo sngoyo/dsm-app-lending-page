ccaf581ffefc7a6565bf13849473bb062c4e163c
sngoyo <salim@kali>
2023-11-30 21:31:30 +0300

    adding file

--- src/redux/actions/photoAction.js
+++ src/redux/actions/photoAction.js
@@ -1,32 +1,10 @@
 export const actionTypes = {
     SET_PHOTOS: "SET_PHOTOS",
-    SELECTED_INDEX: "SELECTED_INDEX",
-    INDEX_REQUESTED: "INDEX_REQUESTED",
-    INDEX_RECEIVED: "INDEX_RECEIVED",
 };
 
 export const setPhotos = (photos) => {
     return {
         type: actionTypes.SET_PHOTOS,
         payload: photos
-    }
-};
-
-
-export const photoAnimate = () => {
-    return async(dispatch) => {
-        dispatch({
-            type: actionTypes.INDEX_REQUESTED
-        });
-
-        let i = 0;
-        while (i <= 5) {
-            dispatch({
-                type: actionTypes.INDEX_RECEIVED,
-                payload: i,
-            })
-
-            i++;
-        }
-    }
+    };
 };
