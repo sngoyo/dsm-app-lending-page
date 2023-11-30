aeec4158e9051156c84ae4e63fe2b114e36f1e6a
sngoyo <salim@kali>
2023-11-30 21:31:03 +0300

    adding

--- src/photoComponent.js
+++ src/photoComponent.js
@@ -1,41 +1,97 @@
 import React from "react";
+import {useEffect, useState, useRef} from "react";
 import { useSelector } from "react-redux";
+import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
 import "./photoComponent.css";
+import classes  from "./photoComponent.css";
+import Slide from "./slides.js";
+
 
 const PhotoComponent = () => {
-    const data = useSelector((state => state.allPhotos));
-    const newIndex = useSelector(state => state);
-    console.log(newIndex.photoIndexReducer.index);
-    const index = newIndex.photoIndexReducer.index
-  const pic_1 = data[1].src
-  const pic_2 = data[2].src
-  const pic_3 = data[3].src
-  const pic_4 = data[4].src
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+    const [slides, setSlides] = useState([]);
+    let image = useSelector((state) => state.allPhotos);
+
+    let slidesArr = [];
+    for(let value in image){
+      slidesArr.push(image[value]);
+    };
+
+
+    //New codes Here: codes for ordering slides
+    useEffect(() => {
+      let new_slides = [];
+      let new_x = 0;
+      let new_y = 0;
+       //order the cars
+        slidesArr.map((slide, index) => {
+              new_x = 255 * index;
+              new_y = 0;
+           new_slides.push(
+               <Slide
+                  key= {index}
+                  image={slide.image}
+                  area={slide.area}
+                  title={slide.title}
+                  x={new_x}
+                  y={new_y}
+                  index={slide.id}
+                  currentSlideIndex={currentSlideIndex}
+                />
+        );
+      })
+        setSlides(new_slides);
+
+
+    },[])
+
+    //New codes ends here
+   useEffect(() =>{
+        const intervalId = setInterval(nextSlide, 3000);
+       return () => clearInterval(intervalId);
+
+    },[]);
+
+
+    //function for sliding to previous slide
+    const previousSlide = () => {
+       setCurrentSlideIndex((currentSlide) =>
+       currentSlide === 0 ? slidesArr.length - 1 : currentSlide - 1);
+    }
+
+     //function for sliding to next slide
+    const nextSlide = () =>{
+      setCurrentSlideIndex((currentSlide) => currentSlide === slidesArr.length - 1 ? 0 : currentSlide + 1);
+    };
 
     return (
-        <div className="photoContainer flex flex-row space-x-7 justify-end mt-80">
-          <div className="div bg-black rounded-3xl "
-          style={{backgroundImage: `url(${pic_1})`, backgroundSize: "cover"}}>
-            <p className="location">{data[1].area}</p>
-            <h3 className="photoDetails">{data[1].title}</h3>
-          </div>
-          <div className="div bg-white rounded-3xl"
-             style={{backgroundImage: `url(${pic_2})`, backgroundSize: "cover"}}>
-            <p className="location">{data[2].area}</p>
-            <h3 className="photoDetails">{data[2].title}</h3>
-          </div>
-          <div className="div bg-white rounded-3xl"
-             style={{backgroundImage: `url(${pic_3})`, backgroundSize: "cover"}}>
-            <p className="location">{data[3].area}</p>
-            <h3 className="photoDetails text-wrap">{data[3].title}</h3>
-          </div>
-          <div className="div bg-white rounded-3xl"
-            style={{backgroundImage: `url(${pic_4})`, backgroundSize: "cover"}}>
-            <p className="location">{data[4].area}</p>
-            <h3 className="photoDetails">{data[4].title}</h3>
-          </div>
+      <div>
+        <div className="sideHeading">
+          <p className="locationHome">{slidesArr[2].area} </p>
+          <p className="photoDetailsHome">{slidesArr[2].title}</p>
+          <p className="otherDetailsHome">Dynamic, coastal, cultural fusion, vibrant markets,
+          historical charm, friendly locals. Dar es Salaam—experience the heartbeat of Tanzania!</p>
         </div>
+
+      <div className="sliderbigContainer" >
+
+          { slides }
+
+          <div className="flex flex-flow absolute left-1/2 bottom-5">
+             <button onClick={previousSlide} className="p-3 mx-10 ml-15 rounded-full shadow bg-white/80">
+             <span><SlArrowLeft /></span>
+            </button>
+          <div  className="bg-white grounded-full">
+         </div>
+           <button onClick={nextSlide} className="p-3 ml-5 rounded-full shadow bg-white">
+            <span><SlArrowRight /></span>
+           </button>
+         </div>
+     </div>
+     </div>
     );
 };
+
+
 
 export default PhotoComponent;
