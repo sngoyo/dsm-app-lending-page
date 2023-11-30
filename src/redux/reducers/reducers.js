178a97af193565a9fe518d29609641e8e43085c6
sngoyo <salim@kali>
2023-11-30 21:32:52 +0300

    adding file

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -7,39 +7,18 @@ const photo_4 = require("./images/victoria_dsm.jpg");
 const photo_5 = require("./images/twiga.jpg");
 
 export const initialState = [
-    { id: 1, src: photo_1, title: "Cheater Chilling", area: "Mikumi - Morogoro" },
-    { id: 2, src: photo_2, title: "CLIMBING MOUNTAIN KILIMANJARO", area: "Moshi - Tanzania" },
-    { id: 3, src: photo_3, title: "Lions at Serengeti National park", area: "Arusha - Tanzania" },
-    { id: 4, src: photo_4, title: "City that never sleeps", area: "Victoria - Dar es salaam" },
-    { id: 5, src: photo_5, title: "Twiga in Ngorongoro Conservation Area", area: "Arusha - Tanzania" }
+    { id: 1, image: photo_1, title: "Cheater Chilling", area: "Mikumi - Morogoro" },
+    { id: 2, image: photo_2, title: "CLIMBING MOUNTAIN KILIMANJARO", area: "Moshi - Tanzania" },
+    // { image: photo_3, title: "Lions at Serengeti National park", area: "Arusha - Tanzania" },
+    { id: 3, image: photo_4, title: "The City that never sleeps", area: "Victoria - Dar es salaam" },
+    { id: 4, image: photo_5, title: "Twiga in Ngorongoro Conservation Area", area: "Arusha - Tanzania" },
 ];
 
-const initialStateForIndex = {
-    index: 0,
-    status: ""
-};
 
 export const photoReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SET_PHOTOS:
             return { state, ...action.payload };
-        case actionTypes.SELECTED_PHOTO:
-            return { state, ...actionTypes.payload };
-        default:
-            return state;
-    };
-};
-
-export const photoIndexReducer = (state = initialStateForIndex, action) => {
-    let newObj = "";
-    switch (action.type) {
-        case actionTypes.INDEX_REQUESTED:
-            newObj = Object.assign({}, state, { status: "waiting" });
-            return newObj;
-        case actionTypes.INDEX_RECEIVED:
-            state.index = action.payload;
-            newObj = Object.assign({}, state, { status: "Received" });
-            return newObj;
         default:
             return state;
     };
